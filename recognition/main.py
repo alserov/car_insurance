6682365100a456019555a57895ec0d733ef8418a
Alex Rokachev <alserokachev@gmail.com>
2024-09-12 17:27:18 +0300

    added route to detect if insurable

--- recognition/main.py
+++ recognition/main.py
@@ -12,8 +12,28 @@ if not os.path.exists(UPLOAD_FOLDER):
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # route for image recognition
-@app.route('/recognition', methods=['POST'])
-def recognition():
+@app.route('/check', methods=['POST'])
+def checkIfIsAbleToBeInsured():
+    if 'image' not in request.files:
+        return jsonify({'error': 'image is not provided'}), 400
+
+    file = request.files['image']
+
+    filename = sf(file.filename)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(file_path)
+
+    mult = random.uniform(0, 1)
+
+    os.remove(file_path)
+
+    if mult > 0.5:
+        return jsonify({'insurable': True})
+
+    return jsonify({'insurable': False})
+
+@app.route('/detect_multiplier', methods=['POST'])
+def detectDamageMultiplier():
     if 'image' not in request.files:
         return jsonify({'error': 'image is not provided'}), 400
 
